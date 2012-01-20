74e79cc482a0ad1f58e20a8cfe318ea3508db2ed
Mantresh <mantreshkhurana@gmail.com>
2012-01-21 00:00:00 +0530

    Initial upload

---
+++ JAVA/NAME/name.java
@@ -0,0 +1,16 @@
+package JAVA.NAME;
+import java.util.Scanner;
+
+public class name
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        
+        System.out.println("Hello " + name);
+        
+        sc.close();
+    }
+}
